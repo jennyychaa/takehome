78896341eba2afe1151fd6ad5f22bacd8f48a5f6
Jenny Cha <jennycha@Jennys-Mac-mini.local>
2023-07-14 17:58:45 -0400

    Added template layout component

---
+++ frontend/src/templates/Layout.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from 'react';
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="container max-w-screen-md mx-auto px-16 py-24">{children}</div>
+  );
+};
+
+export default Layout;
