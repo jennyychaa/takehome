3e8ad808d2cd5d0dcc5858fbfc8c69409095a448
Jenny Cha <jennycha@Jennys-Mac-mini.local>
2023-07-14 14:30:08 -0400

    Implemented Prescriptions API in the backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,33 +1,76 @@
 import express, { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 
+import { Patient, Prescription, PrescriptionStatus } from '@photon-health/models';
+
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-interface Patient {
-  id: string;
-  firstName: string;
-  lastName: string;
-}
-
 interface Database {
   patients: Record<string, Patient>;
-  prescriptions: Record<string, unknown>;
+  prescriptions: Record<string, Prescription>;
 }
 
 const database: Database = {
-  patients: {},
-  prescriptions: {},
+  patients: {
+    '0': {
+      id: '0',
+      firstName: 'Elizabeth',
+      lastName: 'Blackwell',
+    },
+    '1': {
+      id: '1',
+      firstName: 'Edward',
+      lastName: 'Jenner',
+    }
+  },
+  prescriptions: {
+    '0': {
+      id: '0',
+      patientId: '0',
+      status: PrescriptionStatus.Filled,
+      medications: 'Drug C, 500mg',
+      notes: 'Must take 2 times a day for 10 days with food.',
+    },
+    '1': {
+      id: '1',
+      patientId: '0',
+      status: PrescriptionStatus.Pending,
+      medications: 'Drug B, 100mg',
+      notes: '',
+    },
+    '2': {
+      id: '2',
+      patientId: '0',
+      status: PrescriptionStatus.InProgress,
+      medications: 'Drug A, 250mg',
+      notes: 'Must take 3 times a day for 7 days.',
+    },
+    '3': {
+      id: '0',
+      patientId: '1',
+      status: PrescriptionStatus.Pending,
+      medications: 'Drug E, 200mg',
+      notes: '',
+    },
+    '4': {
+      id: '0',
+      patientId: '1',
+      status: PrescriptionStatus.Pending,
+      medications: 'Drug D, 300mg',
+      notes: 'Must use daily until the condition clears up.'
+    },
+  },
 };
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_, res: Response) => {
   res.json({
     message: "hello world!",
   });
 });
 
-app.get("/patients", (req: Request, res: Response) => {
+app.get("/patients", (_, res: Response) => {
   res.json(Object.values(database.patients));
 });
 
@@ -41,17 +84,51 @@ app.get("/patients/:id", (req: Request, res: Response) => {
 });
 
 app.post("/patients", (req: Request, res: Response) => {
-  const { firstName, lastName } = req.body || {};
-  if (!firstName || !lastName) {
+  const { firstName, lastName, medications, notes } = req.body || {};
+
+  if (!firstName || !lastName || !medications) {
     res.status(400).send("Error: Missing required fields");
   } else {
-    const id = uuidv4();
-    database.patients[id] = {
-      id,
+    const patientId = uuidv4();
+    const prescriptionId = uuidv4();
+
+    database.patients[patientId] = {
+      id: patientId,
       firstName,
       lastName,
     };
-    res.json(database.patients[id]);
+    database.prescriptions[prescriptionId] = {
+      id: prescriptionId,
+      patientId,
+      status: PrescriptionStatus.Pending,
+      medications,
+      notes: notes || '',
+    };
+
+    res.json(database.patients[patientId]);
+  }
+});
+
+app.get("/prescriptions", (_, res: Response) => {
+  res.json(Object.values(database.prescriptions));
+});
+
+app.patch("/prescriptions/status/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { status } = req.body || {};
+
+  if (database.prescriptions[id]) {
+   if (Object.values(PrescriptionStatus).includes(status)) {
+      database.prescriptions[id] = {
+        ...database.prescriptions[id],
+        status,
+      }
+      res.json(database.prescriptions[id]);
+   } else {
+    res.status(400).send("Error: Invalid status type");
+   }
+  } else {
+    res.sendStatus(404);
   }
 });
 
