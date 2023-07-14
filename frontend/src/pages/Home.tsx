863fdeda1028b6e309054cdedfd9153663404dcd
Jenny Cha <jennycha@Jennys-Mac-mini.local>
2023-07-14 17:58:59 -0400

    Created and styled the home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import Layout from '../templates/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1 className="text-5xl mb-16">Select user type</h1>
+      <div className="flex flex-col gap-8">
+        <Link
+          className="rounded-md bg-rose-200 hover:bg-rose-300 px-8 py-8 text-2xl"
+          to="/pharmacist"
+        >
+          I am a <strong>Pharmacist</strong> 💊
+        </Link>
+        <Link
+          className="rounded-md bg-sky-200 hover:bg-sky-300 px-8 py-8 text-2xl"
+          to="/provider"
+        >
+          I am a <strong>Provider</strong> 🩺
+        </Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
